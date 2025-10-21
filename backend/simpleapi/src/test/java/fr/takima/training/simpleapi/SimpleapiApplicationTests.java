72cb292b0ad516585fe2819fc7ac49b0805f59fb
manon <manon.bacque@epfedu.fr>
2025-10-21 14:09:40 +0200

    Code fin tp1

---
+++ backend/simpleapi/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.simpleapi.src.test.java.fr.takima.training.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
