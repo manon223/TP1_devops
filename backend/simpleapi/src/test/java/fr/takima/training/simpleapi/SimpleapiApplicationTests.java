be12c7930909b38c4aca268698e2dff7814a5ed5
manon <manon.bacque@epfedu.fr>
2025-10-21 14:58:26 +0200

    Modification du chemin du test

--- backend/simpleapi/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
+++ backend/simpleapi/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
@@ -1,4 +1,4 @@
-package backend.simpleapi.src.test.java.fr.takima.training.simpleapi;
+package fr.takima.training.simpleapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
